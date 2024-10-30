00b9cb9aced6d3d4c6853fa72078da6c6796a32b
Salih <salihmorina555@gmail.com>
2024-10-30 13:40:09 +0100

    Skapar menyn.

--- Twitter_Projekt/Program.cs
+++ Twitter_Projekt/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+
 
 namespace Twitter_Projekt
 {
     class Program
     {
-        static void Main(string[] args)
+
+        
+        // 1 Skapa en inmatningsruta för tweets
+
+        //2. Skapa en lista för att visa tweets
+
+        //3. Spara och ladda tweets
+
+        //4. Enkel användarhantering
+
+        //5. Skapa konto/login
+
+        //6. Testa att posta och visa tweets
+        public void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Välj ett av följande alternativ");
+            Console.WriteLine("1: Skapa ett inlägg");
+            Console.WriteLine("2: Visa alla tweets");
+            Console.WriteLine("3: Ta bort tweet");
+            Console.WriteLine("4: Skapa konto");
+            Console.WriteLine("5: Logga in");
+
+
+
+
         }
     }
 }
