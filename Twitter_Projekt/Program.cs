371c2e41a48174830a5eb79259ab7ab2511c6ecb
Salih <salihmorina555@gmail.com>
2024-10-30 14:25:55 +0100

    Vi har skapat DeleteTweet och fixat till lite med ShowAllPost Så att det kommer ett nummer för varje tweet.

--- Twitter_Projekt/Program.cs
+++ Twitter_Projekt/Program.cs
@@ -9,40 +9,70 @@ namespace Twitter_Projekt
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Välj ett av följande alternativ");
-            Console.WriteLine("1: Skapa ett inlägg");
-            Console.WriteLine("2: Visa alla tweets");
-            Console.WriteLine("3: Ta bort tweet");
-            Console.WriteLine("4: Skapa konto");
-            Console.WriteLine("5: Logga in");
 
-            int chooise = int.Parse(Console.ReadLine());
-
-            switch (chooise)
+            while (true)
             {
-                case 1:
-                    CreatePost();
-                    break;
-                case 2:
-                    ShowAllPost();
-                    break;
+                Console.WriteLine("Välj ett av följande alternativ");
+                Console.WriteLine("1: Skapa ett inlägg");
+                Console.WriteLine("2: Visa alla tweets");
+                Console.WriteLine("3: Ta bort tweet");
+                Console.WriteLine("4: Skapa konto");
+                Console.WriteLine("5: Logga in");
+
+                int chooise = int.Parse(Console.ReadLine());
+
+                switch (chooise)
+                {
+                    case 1:
+                        CreatePost();
+                        break;
+                    case 2:
+                        ShowAllPost();
+                        break;
+
+                    case 3:
+                        DeleteTweet();
+                        break;
+                }
+
+               
             }
         }
 
         public static void ShowAllPost()
         {
             Console.WriteLine("Här kommer alla inlägg");
+            int i = 1;
             foreach (string post in listofposts)
             {
-                Console.WriteLine(post);
+                Console.WriteLine($"{i}.{ post}");
+                i++;
             }
         }
-
-        private static void CreatePost()
+        public static void CreatePost()
         {
             Console.WriteLine("Vad vill du dela med dig utav?");
             string post = Console.ReadLine();
             listofposts.Add(post);
+    
+        }
+
+        //public static void CreateAccoount()
+        //{
+        //    Console.Write("Vänligen ange ditt användernamn: ");
+        //    string userName = Console.ReadLine();
+
+        //    Console.Write("Vänligen ange ditt lösenord: ");
+        //    string passWord = Console.ReadLine();
+        //}
+
+        public static void DeleteTweet()
+        {
+            Console.WriteLine("Skriv vilket inlägg du vill ta bort");
+            ShowAllPost();
+            int removePost = int.Parse(Console.ReadLine());
+            listofposts.RemoveAt(removePost - 1);
+            Console.WriteLine($"Du tog bort {removePost}");
         }
     }
 }
