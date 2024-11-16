54824b2cb668621af1ae36f31d529f6af6db57fd
djezaahmad@gmail.com <djezaahmad@gmail.com>
2024-11-17 00:18:55 +0100

    Stylar om menyn.

--- Twitter_Projekt/MenuManagment.cs
+++ Twitter_Projekt/MenuManagment.cs
@@ -2,30 +2,47 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
-using System.Windows;
 
 namespace Twitter_Projekt
 {
     internal class MenuManagment
     {
+        private static void PrintMenuHeader(string title)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("=================================================");
+            Console.WriteLine($"         {title.ToUpper()}");
+            Console.WriteLine("=================================================");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        }
+
+        private static void PrintMenuOption(int number, string text, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"| {number.ToString().PadLeft(2)}: {text.PadRight(25)}|");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        private static void PrintSeparator()
+        {
+            Console.WriteLine("=================================================");
+        }
+
         public static void HandleLoginMenu()
         {
-            bool isRunnning = true;
-            while (isRunnning)
+            bool isRunning = true;
+
+            while (isRunning)
             {
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 1: Skapa Konto        |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 2: Logga in           |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 3: Inställningar      |");
-                Console.WriteLine(" -----------------------");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 4: Avsluta Programmet |");
-                Console.WriteLine(" -----------------------");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
+                PrintMenuHeader("Login Menu");
+
+                PrintMenuOption(1, "Skapa Konto");
+                PrintMenuOption(2, "Logga in");
+                PrintMenuOption(3, "Inställningar");
+                PrintMenuOption(4, "Avsluta Programmet", ConsoleColor.Red);
 
                 try
                 {
@@ -33,109 +50,81 @@ namespace Twitter_Projekt
                 }
                 catch
                 {
-                    Console.WriteLine("Det måste vara en siffra");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("⚠️ Det måste vara en siffra. Försök igen!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
                 }
 
-                if (LoginManagment.loginChooise == 1)
+                switch (LoginManagment.loginChooise)
                 {
-                    UserManagment.CreateAccount();
-                }
-                else if (LoginManagment.loginChooise == 2)
-                {
-                    if (LoginManagment.Login())
-                    {
-                        Console.WriteLine($"Välkommen, {LoginManagment.loggedInUsername}!");
+                    case 1:
+                        UserManagment.CreateAccount();
+                        break;
+                    case 2:
+                        if (LoginManagment.Login())
+                        {
+                            Console.WriteLine($"Välkommen, {LoginManagment.loggedInUsername}!");
+                            Thread.Sleep(2000);
+                            isRunning = false;
+                        }
+                        break;
+                    case 3:
+                        UserManagment.Theme();
+                        break;
+                    case 4:
+                        Console.WriteLine("Programmet avslutas nu.");
                         Thread.Sleep(2000);
-                        isRunnning = false;
-
-                    }
-                }
-                else if (LoginManagment.loginChooise == 3)
-                {
-                    UserManagment.Theme();
-                   
-                }
-                else if (LoginManagment.loginChooise == 4)
-                {
-
-                    Console.WriteLine("Programmet avslutas nu.");
-                    Thread.Sleep(2000);
-                    isRunnning = false;
-                    Environment.Exit(0);
-                }
-
-                else
-                {
-                    Console.WriteLine("Vänligen ange ett giltigt val!");
+                        isRunning = false;
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("⚠️ Vänligen ange ett giltigt val!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
                 }
             }
         }
 
         public static void HandleMenu()
         {
-            bool error = false;
             bool runProgram = true;
-            //AdManagment.Ad();
-
 
             while (runProgram)
             {
+                PrintMenuHeader("Main Menu");
 
-                Console.Clear();
+                PrintMenuOption(1, "Skapa ett inlägg");
+                PrintMenuOption(2, "Visa alla tweets");
+                PrintMenuOption(3, "Ta bort tweet");
+                PrintMenuOption(4, "Sök efter följare");
+                PrintMenuOption(5, "Retweeta");
+                PrintMenuOption(6, "Skicka DM");
+                PrintMenuOption(7, "Visa mina följare");
+                PrintMenuOption(8, "Logga ut");
+                PrintMenuOption(9, "Redigera inlägg");
+                PrintMenuOption(10, "Lika/dislika inlägg");
 
-                if (error == true)
+                PrintSeparator();
+                PrintMenuOption(11, "Avsluta programmet", ConsoleColor.Red);
+
+                Console.WriteLine();
+
+                int choice = 0;
+                try
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Det måste vara ett nummer!");
-                    Console.ForegroundColor = ConsoleColor.White;
+                    choice = int.Parse(Console.ReadLine());
                 }
-
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Välj ett av följande alternativ");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 1: Skapa ett inlägg   |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 2: Visa alla tweets   |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 3: Ta bort tweet      |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 4: Sök efter följare  |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 5: Retweeta           |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 6: Skicka DM          |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 7: Visa mina följare  |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 8: Logga ut           |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("|9: Redigera inlägg     |");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("|10: Lika/dislika inlägg |");
-                Console.WriteLine(" -----------------------");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(" -----------------------");
-                Console.WriteLine("| 11: Avsluta programmet |");
-                Console.WriteLine(" -----------------------");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
-
-                Console.WriteLine();
-
-
-                int chooise = 0;
-                try { chooise = int.Parse(Console.ReadLine()); }
                 catch
                 {
-                    error = true;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("⚠️ Det måste vara ett nummer! Försök igen.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    continue;
                 }
 
-                switch (chooise)
+                switch (choice)
                 {
                     case 1:
                         PostManagment.CreatePost();
@@ -154,20 +143,22 @@ namespace Twitter_Projekt
                         {
                             PostManagment.Reposta();
                         }
-                        else if (PostManagment.listofposts.Count < 1)
+                        else
                         {
-                            Console.WriteLine("finns inga inlägg att reposta!");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Det finns inga inlägg att retweeta!");
+                            Console.ForegroundColor = ConsoleColor.White;
                         }
-                        break;
-                    case 7:
-                        UserManagment.ShowUserInfo();
                         break;
                     case 6:
                         MessageManagment.SendDirectMessage();
                         break;
+                    case 7:
+                        UserManagment.ShowUserInfo();
+                        break;
                     case 8:
                         LoginManagment.Logout();
-                        break;
+                        return;
                     case 9:
                         PostManagment.EditPost();
                         break;
@@ -175,13 +166,19 @@ namespace Twitter_Projekt
                         PostManagment.Like_Dislike();
                         break;
                     case 11:
-                        runProgram = false;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Programmet avslutas nu.");
                         Thread.Sleep(2000);
                         Environment.Exit(0);
                         break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("⚠️ Ogiltigt val! Försök igen.");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
                 }
 
+                Console.WriteLine("\nTryck på valfri tangent för att fortsätta...");
                 Console.ReadKey();
             }
         }
