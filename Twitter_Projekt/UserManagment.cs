d5d20a1fe804e047ad8769a06a0b3f7f04bdae0a
djezaahmad@gmail.com <djezaahmad@gmail.com>
2024-11-16 21:47:29 +0100

    Fixar validering när du skapar konto.

--- Twitter_Projekt/UserManagment.cs
+++ Twitter_Projekt/UserManagment.cs
@@ -13,82 +13,80 @@ namespace Twitter_Projekt
         public string Password { get; set; }
         public List<string> Followers { get; set; } = new List<string>();
 
-        public static List<UserManagment> users =LoadUsers();
+        public static List<UserManagment> users = LoadUsers();
         public static void CreateAccount()
         {
-            Console.Write("Ange ett användarnamn: ");
-            string username = Console.ReadLine();
+           
+                Console.Write("Ange ett användarnamn: ");
+                string username = Console.ReadLine();
 
-            Console.Write("Ange ett lösenord: ");
-            string password = LoginManagment.ReadPassword();
-            while (password.Length < 6 || !password.Any(char.IsDigit) || !password.Any(char.IsLetter))
-            {
-                Console.WriteLine("Lösenordet måste vara minst 6 tecken långt och innehålla både siffror och bokstäver, Försök igen!");
-                Console.Write("Ange ett lösenord: ");
-                password = Console.ReadLine();
-            }
-
-            Console.WriteLine("Ange din e-postadress: ");
-            string email = Console.ReadLine();
-            while (!IsValidEmail(email))
-            {
-                Console.WriteLine("Ogiltig e-postadress. Försök igen: ");
-                email = Console.ReadLine();
-            }
-
-
-
-            Console.WriteLine("Ange ditt förnamn: ");
-            string firstname = Console.ReadLine();
-            while (string.IsNullOrEmpty(firstname))
-            {
-                Console.WriteLine("Förnamn får inte vara tomt. Ange ditt förnamn igen");
-                Console.Write("Ange ditt förnamn: ");
-                firstname = Console.ReadLine();
-            }
-
-            Console.WriteLine("Ange ditt efternamn: ");
-            string lastname = Console.ReadLine();
-            while (string.IsNullOrEmpty(lastname))
-            {
-                Console.WriteLine("Efternamn får inte vara tomt. Ange ditt förnamn igen");
-                Console.Write("Ange ditt efternamn: ");
-                lastname = Console.ReadLine();
-            }
-
-            Console.WriteLine("Välj kön (1 - Man, 2 - Kvinna, 3 - Annat) : ");
-            string genderInput = Console.ReadLine();
-            string gender = "";
-            while (genderInput != "1" && genderInput != "2" && genderInput != "3")
-            {
-                Console.WriteLine("Ogiltigt val. Välj 1 för Man, 2 för Kvinna eller 3 för annat");
-                genderInput = Console.ReadLine();
-            }
-            switch (genderInput)
-            {
-                case "1": gender = "Man"; break;
-                case "2": gender = "Kvinna"; break;
-                case "3": gender = "Annat"; break;
-            }
-
-
-
-            foreach (UserManagment user in users)
-            {
-                if (user.Username == username)
+                foreach (UserManagment user in users)
                 {
-                    Console.WriteLine("Användarnamnet är upptaget. Försök igen.");
-                    return;
+                    if (user.Username == username)
+                    {
+                        Console.WriteLine("Användarnamnet är upptaget. Försök igen.");
+                        return;
+                    }
                 }
+
+                Console.Write("Ange ett lösenord: ");
+                string password = LoginManagment.ReadPassword();
+                while (password.Length < 6 || !password.Any(char.IsDigit) || !password.Any(char.IsLetter))
+                {
+                    Console.WriteLine("Lösenordet måste vara minst 6 tecken långt och innehålla både siffror och bokstäver, Försök igen!");
+                    Console.Write("Ange ett lösenord: ");
+                    password = Console.ReadLine();
+                }
+
+                Console.WriteLine("Ange din e-postadress: ");
+                string email = Console.ReadLine();
+                while (!IsValidEmail(email))
+                {
+                    Console.WriteLine("Ogiltig e-postadress. Försök igen: ");
+                    email = Console.ReadLine();
+                }
+
+
+
+                Console.WriteLine("Ange ditt förnamn: ");
+                string firstname = Console.ReadLine();
+                while (string.IsNullOrEmpty(firstname))
+                {
+                    Console.WriteLine("Förnamn får inte vara tomt. Ange ditt förnamn igen");
+                    Console.Write("Ange ditt förnamn: ");
+                    firstname = Console.ReadLine();
+                }
+
+                Console.WriteLine("Ange ditt efternamn: ");
+                string lastname = Console.ReadLine();
+                while (string.IsNullOrEmpty(lastname))
+                {
+                    Console.WriteLine("Efternamn får inte vara tomt. Ange ditt förnamn igen");
+                    Console.Write("Ange ditt efternamn: ");
+                    lastname = Console.ReadLine();
+                }
+
+                Console.WriteLine("Välj kön (1 - Man, 2 - Kvinna, 3 - Annat) : ");
+                string genderInput = Console.ReadLine();
+                string gender = "";
+                while (genderInput != "1" && genderInput != "2" && genderInput != "3")
+                {
+                    Console.WriteLine("Ogiltigt val. Välj 1 för Man, 2 för Kvinna eller 3 för annat");
+                    genderInput = Console.ReadLine();
+                }
+                switch (genderInput)
+                {
+                    case "1": gender = "Man"; break;
+                    case "2": gender = "Kvinna"; break;
+                    case "3": gender = "Annat"; break;
+                }
+
+                UserManagment newUser = new UserManagment { Username = username, Password = password };
+                users.Add(newUser);
+
+                SaveUsers();
+                Console.WriteLine("Konto skapat!");
             }
-
-            UserManagment newUser = new UserManagment { Username = username, Password = password };
-            users.Add(newUser);
-
-            SaveUsers();
-            Console.WriteLine("Konto skapat!");
-        }
-
         public static bool IsValidEmail(string email)
         {
             try
