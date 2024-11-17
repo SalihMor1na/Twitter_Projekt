dd539eb64ec8f5d39d031ef7d9f02b37ea241bfc
Salih <salihmorina555@gmail.com>
2024-11-17 17:59:49 +0100

    Fixar en todolist

---
+++ Twitter_Projekt/ToDoList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twitter_Projekt
+{
+    internal class ToDoList
+    {
+        /*
+         Skapa inlägg
+         kunna se alla tweets
+         kunna ta bort tweets
+         kunna söka efter följare
+         kunna retweeta
+         kunna skicka dm
+         se sina följare
+         kunna logga ut
+         kunna logga in
+         ska gå att skapa konto
+         ska gå att fixa inställningar
+         kunna lika och dislika
+         kunna redigera inlägg
+         reklam ska komma upp
+          
+         */
+
+    }
+}
